615bb96d7c03446398b4eda2bf15930dda3d88d2
alexsantosfilho <tudomaistv@gmail.com>
2016-10-31 15:59:29 -0200

    Add o inimigo com a AI

---
+++ Source/MyProject/MyAIController.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+
+public:
+
+	AMyAIController();
+	virtual void Possess(APawn* Pawn) override;
+	uint8 EnemyKeyID;
+
+private:
+	UBlackboardComponent* BlackboardComp;
+	UBehaviorTreeComponent* BehaviorComp;
+
+
+};
